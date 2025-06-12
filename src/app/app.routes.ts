e0db74f2cebca9cf8031e4c3ab130aed62278d89
Honeybadger-png <9emertgul@gmail.com>
2025-06-12 21:42:16 +0300

    new project i think files are good enough to start for now

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { BookListComponent } from './book-list/book-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: "", component: BookListComponent}
+];
