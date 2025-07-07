540e366dc0c1bbcfbe1d7eea882a27da65b4aec9
Honeybadger-png <9emertgul@gmail.com>
2025-07-07 15:09:12 +0300

    working on the structure of homepage

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -10,4 +10,43 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
 
+  newBlogs = [
+    {
+      id : 1
+    },
+    {
+      id : 2
+    },
+    {
+      id : 3
+    },
+    {
+      id : 4
+    },
+  ];
+
+  currentSeasonBooks = [
+    {
+      id: 1,
+    },
+    {
+      id: 2,
+    },
+    {
+      id: 3,
+    },
+    {
+      id: 4,
+    },
+    {
+      id: 5,
+    },
+    {
+      id: 6,
+    },
+    {
+      id: 7,
+    }
+  ]
+
 }
