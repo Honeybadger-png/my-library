db1d69c601824b7f481bd7259dd4837f296a56ad
Honeybadger-png <9emertgul@gmail.com>
2025-07-07 14:12:46 +0300

    creating a home page decided to design it first then I will try to make an api for the project

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,8 +2,10 @@ import { Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { BookListComponent } from './book-list/book-list.component';
 import { BookFormComponent } from './book-form/book-form.component';
+import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
-    {path: "", component: BookListComponent},
+    {path: "", component: HomeComponent},
+    {path: "my-library", component: BookListComponent},
     {path: "books", component:BookFormComponent}
 ];
--- src/app/book-form/book-form.component.ts
+++ src/app/book-form/book-form.component.ts
@@ -1,13 +1,25 @@
 import { Component } from '@angular/core';
-import { Location } from '@angular/common';
+import { DatePipe, Location } from '@angular/common';
+import {FormGroup, FormControl, ReactiveFormsModule, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-book-form',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './book-form.component.html',
   styleUrl: './book-form.component.css'
 })
 export class BookFormComponent {
+
+  bookForm = new FormGroup({
+    title: new FormControl(''),
+    author: new FormControl(''),
+    length: new FormControl(''),
+    status: new FormControl(''),
+    language: new FormControl(''),
+    mainImage: new FormControl<null | File>(null, {validators: [Validators.required]}),
+    addedDate : new FormControl<null | String>(null),
+    updatedDate : new FormControl<null | String>(null)
+  })
 
   constructor(private location: Location){
 
@@ -15,5 +27,21 @@ export class BookFormComponent {
 
   goBack():void{
     this.location.back();
+  }
+
+  onSubmit(){
+    const now = Date.now();
+    const pipe = new DatePipe("en-US");
+    const formattedDate = pipe.transform(now,'short');
+    this.bookForm.patchValue({addedDate:formattedDate,updatedDate:formattedDate})
+    console.log(this.bookForm.value);
+  }
+
+  imageSelected(event: Event){
+    const input = event.target as HTMLInputElement;
+    if(input.files && input.files.length > 0){
+      const file: File = input.files[0];
+      this.bookForm.patchValue({mainImage: file});
+    }
   }
 }
